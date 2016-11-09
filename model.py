cb043236ec6f930823ef8ef2078ae03d8d26e390
Anand Bhoraskar <anandbhoraskar@gmail.com>
2016-11-09 07:57:39 +0530

    added kmeans + getdata uses pickle

--- model.py
+++ model.py
@@ -3,6 +3,7 @@ import cPickle as pickle
 import numpy as np
 from Song import Song
 
+
 def fillSongs(numNeeded):
     rocksongs = []
     jazzsongs = []
@@ -46,17 +47,27 @@ def fillSongs(numNeeded):
                         if (len(currsong.featureVectors) > 0):
                             rocksongs.append(currsong)
 
-    return rocksongs[:numNeeded], jazzsongs[:numNeeded]
+    return rocksongs, jazzsongs
 
 
 def getData(numPerGenre):
-    rocksongs, jazzsongs = fillSongs(numPerGenre)
+    if os.path.exists("jazz.pkl") and os.path.exists("rock.pkl"):
+        with open('jazz.pkl', 'rb') as inp:
+            jazzsongs = pickle.load(inp)
+            print 'Loaded jazz songs'
+        with open('rock.pkl', 'rb') as inp:
+            rocksongs = pickle.load(inp)
+            print 'Loaded rock songs'
+    else:
+        rocksongs, jazzsongs = fillSongs(numPerGenre)
     return rocksongs, jazzsongs
     assert (len(rocksongs) == numPerGenre and len(jazzsongs) == numPerGenre)  # if false then need more data
+
 
 def songToInput(song):
     return song.meanVector + \
            [item for sublist in song.covarianceMatrix for item in sublist]
+
 
 def songToResultVector(song):
     if song.genre == 'jazz':
@@ -66,8 +77,8 @@ def songToResultVector(song):
     else:
         return 2
 
-def main():
-    metalsongs, jazzsongs = getData(250)
+    # def main():
+    metalsongs, jazzsongs = getData(1000)
     output = open('jazz.pkl', 'wb')
     pickle.dump(jazzsongs, output)
     output.close()
@@ -75,9 +86,9 @@ def main():
     pickle.dump(metalsongs, output)
     output.close()
 
-    with open('jazz.pkl', 'rb') as inp:
+    with open('jazz1.pkl', 'rb') as inp:
         jazzsongs = pickle.load(inp)
-    with open('metal.pkl', 'rb') as inp:
+    with open('metal1.pkl', 'rb') as inp:
         metalsongs = pickle.load(inp)
 
     training_input = np.asarray([songToInput(song) for song in metalsongs[:200] + jazzsongs[:200]])
@@ -86,5 +97,5 @@ def main():
     test_input = np.asarray([songToInput(song) for song in metalsongs[200:] + jazzsongs[200:]])
     test_result = np.asarray([songToResultVector(song) for song in metalsongs[200:] + jazzsongs[200:]])
 
-if __name__ == "__main__":
-    main()
+    # if __name__ == "__main__":
+    #     main()
