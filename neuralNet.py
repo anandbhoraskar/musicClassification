7ce39bf549fce50123999a99d67ecccf78c6342c
Maitreyee <maitreyeemulawkar@gmail.com>
2016-11-19 01:17:56 +0530

    Adding code for neural network

---
+++ neuralNet.py
@@ -0,0 +1,71 @@
+import h5py
+import os
+import random
+import numpy as np
+import model
+
+from sklearn.neural_network import MLPClassifier
+
+
+# def randomSampleFeatures(song, numSamples):
+#     chosen = []
+#     remaining = len(song.featureVectors)
+#     for i in range(len(song.featureVectors)):
+#         prob = numSamples / float(remaining)
+#         if random.random() < prob:
+#             chosen.append(song.featureVectors[i])
+#             numSamples -= 1
+#         remaining -= 1
+#     return chosen
+
+def randomSampleFeatures(song, numSamples):
+    chosen = []
+    for i, fv in enumerate(song.featureVectors):
+        if i < numSamples:
+            chosen.append(fv)
+        elif i >= numSamples and random.random() < numSamples / float(i + 1):
+            replace = random.randint(0, len(chosen) - 1)
+            chosen[replace] = fv
+    return chosen
+
+
+def getFeatureVectors(songs, perSong):
+    featureVectors = np.empty((0, perSong * 12))
+    for song in songs:
+        fv = randomSampleFeatures(song, perSong)
+        if (len(fv) == perSong):
+            featureVectors = np.append(featureVectors, np.reshape(fv, (1, perSong * 12)), axis=0)
+    return featureVectors
+
+
+def getTrainingData(rocksongs, jazzsongs, perSong, div):
+    rockfeatures = getFeatureVectors(rocksongs, perSong)
+    jazzfeatures = getFeatureVectors(jazzsongs, perSong)
+    trainRock = rockfeatures[:div]
+    testRock = rockfeatures[div:]
+    trainJazz = jazzfeatures[:div]
+    testJazz = jazzfeatures[div:]
+
+    def shuffle(R, J):
+        X = np.empty((0, perSong * 12))
+        X = np.append(X, R, axis=0)
+        X = np.append(X, J, axis=0)
+        Y = [0] * len(R) + [1] * len(J)
+        data = zip(X, Y)
+        random.shuffle(data)
+        X, Y = zip(*data)
+        return [X, Y]
+
+    training_data = shuffle(trainRock, trainJazz)
+    testing_data = shuffle(testRock, testJazz)
+    return training_data, testing_data
+
+
+rocksongs, jazzsongs = model.getData(250)
+training_data, testing_data = getTrainingData(rocksongs, jazzsongs, 90, 200)
+clf = MLPClassifier(solver='lbfgs', hidden_layer_sizes=(1080 * 2, 1080, 90, 30))
+
+clf.fit(training_data[0], training_data[1])
+pred = clf.predict(testing_data[0])
+
+sum(testing_data[1] == pred)
